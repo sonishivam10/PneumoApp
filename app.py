2a0f90721d5da8edd368a6b0d0efb441cb5110ba
ShivamS <sonishivama@gmail.com>
2020-12-14 20:06:54 +0530

    first commit

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ st.title("Curae PnuemoApp")
 logo=Image.open('REALAI.jpg')
 
 st.header("AI supported Pneumonia Detection App!")
-st.markdown(unsafe_allow_html=True, body="<p>Pneumonia is an infection that inflames the alveoli in one or both lungs. 
+st.markdown(unsafe_allow_html=True, body="<p>Pneumonia is an infection that inflames the alveoli in one or both lungs. "
 						"The alveoli may fill with pus, causing cough with phlegm or pus, fever, chills, and difficulty breathing." 
 						"In general, pneumonia is caused by a virus, a bacteria or fungi."
 						"Pneumonia can range in seriousness from mild to life-threatening."
 						"It is most serious for infants and young children, people older than age 65,"
 						"and people with health problems or weakened immune systems."
-						"Chest X-Ray images can be useful in the diagnosis of Pneumonia. A doctor can diagnose it, 
+						"Chest X-Ray images can be useful in the diagnosis of Pneumonia. A doctor can diagnose it, "
 						"know the extent and the location of the infection by looking at an X-Ray image withing a few seconds or minutes."
 						"The aim of this work is to automatize the detection of Pneumonia by using Deep Learning algorithms and only X-Ray images."
  						"Different techniques will be used in order to achieve the best possible results. "
